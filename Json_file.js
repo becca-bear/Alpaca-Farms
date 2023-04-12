5bf97f88e26346ff3d3038a998334cff381eb507
Rebecca D <rebeccajoann7@gmail.com>
2023-04-12 12:29:17 -0400

    first commit

---
+++ Json_file.js
@@ -0,0 +1,29 @@
+Convert a json file to pandas dataframe; the json file looks like this:
+{
+    "farms": [
+        {
+            "id": "7398",
+            "name": "High Country Alpacas LLC",
+            "addressOne": "4128 County Road 129",
+            "addressTwo": "",
+            "city": "Hesperus",
+            "state": "CO",
+            "zip": "81326",
+            "email": "mary.wilcox2001@gmail.com",
+            "phone": "+480-296-8588",
+            "website": "www.highcountryalpacas.com",
+            "lat": 37.17279052734375,
+            "lng": -108.0923080444336
+        },
+        {
+            "id": "8366",
+            "name": "Tiskilwa Farms Alpacas",
+            "city": "Tiskilwa",
+            "state": "IL",
+            "zip": "61368",
+            "phone": "+312-342-9855",
+            "lat": 41.30712890625,
+            "lng": -89.62848663330078
+        },
+    ]
+}
